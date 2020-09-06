5deb11eb025527a79f3b0aa13ddb8a9c7a4f2de8
Mithun <mithunrathinasamy@live.com>
2020-09-06 23:49:54 +1000

    feat: update App.js for claims table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { getClaims } from "./api";
+import { Row, Column } from "./components";
+import { format } from "./utils/date";
 
 function App() {
-  return <></>;
+  const [claims, setClaims] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      setClaims(await getClaims());
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Row header>
+        <Column>Claimant</Column>
+        <Column>Assigned</Column>
+        <Column>Status</Column>
+        <Column>Created</Column>
+        <Column>Updated</Column>
+      </Row>
+      {claims.map((claim) => (
+        <Row key={claim.id}>
+          <Column>{claim.claimant}</Column>
+          <Column>{claim.assignedTo}</Column>
+          <Column>{claim.status}</Column>
+          <Column>{format(claim.createdAt)}</Column>
+          <Column>{format(claim.updatedAt)}</Column>
+        </Row>
+      ))}
+    </>
+  );
 }
 
 export default App;
