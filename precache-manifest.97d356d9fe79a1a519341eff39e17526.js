18b0f4af565882171bceae00a5cc6bc0e479151f
mithuniverse <mithuniverse@users.noreply.github.com>
2020-09-06 04:49:13 +0000

    deploy: b7923908cccc3173d5ff72284685bc769151f41c

---
+++ precache-manifest.97d356d9fe79a1a519341eff39e17526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6346723924ec0195cc9a0ad482bdda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bba6e71751e75d7a025",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe0f8ca135249b7b74c5",
+    "url": "/static/js/2.d4360236.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4360236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bba6e71751e75d7a025",
+    "url": "/static/js/main.f348e2c8.chunk.js"
+  },
+  {
+    "revision": "15b31300ebd53a56f9ed",
+    "url": "/static/js/runtime-main.26c4be42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
