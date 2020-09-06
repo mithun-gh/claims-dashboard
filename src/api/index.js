5afd53db58ba56073cfa3b7b66e7b41ac14b2242
Mithun <mithunrathinasamy@live.com>
2020-09-06 23:30:36 +1000

    feat: add SLA sorting

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,18 @@ import { API_URL_BASE } from "./config";
 import { toQueryString } from "./utils";
 
 export const getSLAs = async (params) => await get("/slas", params);
-export const getClaims = async (params) => await get("/claims", params);
+
+export const getClaims = async (params) => {
+  const claims = await get("/claims", params);
+
+  const hoursExceedingSLA = (claim) => {
+    const today = new Date();
+    const updated = new Date(claim.updatedAt);
+    return Math.abs(updated - today) / 3.6e6;
+  };
+
+  return claims.sort((a, b) => hoursExceedingSLA(b) - hoursExceedingSLA(a));
+};
 
 async function get(url, params) {
   const response = await fetch(`${API_URL_BASE}${url}${toQueryString(params)}`);
